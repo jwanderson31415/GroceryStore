325628b39b823b9539f7c0a4e8c3c83f20ac5d89
coconutshrimp <jwanderson31415@gmail.com>
2023-09-20 14:35:15 -0400

    Update Table

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,13 +1,40 @@
 import Controller.Controller;
 import Util.ConnectionSingleton;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 //        this line is just for testing that your tables get set up correctly
 //        if not, you'll get a stack trace
-        ConnectionSingleton.getConnection();
+        //ConnectionSingleton.getConnection();
 //        this line is for starting the javalin server
-        Controller controller = new Controller();
-        controller.getAPI().start();
+        //Controller controller = new Controller();
+        //controller.getAPI().start();
+
+        Connection conn = ConnectionSingleton.getConnection();
+        PreparedStatement ps1 = conn.prepareStatement("insert into table1 (column1, column2) values (2, 'test2')')");
+        ps1.executeUpdate();
+        PreparedStatement ps2 = conn.prepareStatement("select * from table1");
+        ResultSet rs2 = ps2.executeQuery();
+
+        while(rs2.next()){
+            System.out.println(rs2.getInt("column1"));
+            System.out.println(rs2.getString("column2"));
+
+        }
     }
 }
+
+
+/**
+ * sql vs no sql
+ * sql has the ability to strictly define tables and relate tables to each ohter
+ * (referential integrity, eg a 'teachers' table may be related logically to a 'classrooms' table)
+ *
+ * noSQL is more scalable and more easily converted into a distributed system than sql
+ * however
+ */
--- src/main/resources/Tables.sql
+++ src/main/resources/Tables.sql
@@ -1,3 +1,3 @@
---h2 is typically used to setup a test database, not a prod database.
---first, drop your tables (to reset your database for testing)
---then create your tables
+drop table table1 if exists;
+create table table1( column1 varchar(255), column2 double, column3 int));
+insert into table1 (column1, column2, column3) values ('Banana', 0.99, 5;)

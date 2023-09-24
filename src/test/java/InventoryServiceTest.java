b60c6005d9ad0894fd81ae1e1cf846c2ba533a34
dqqzz <zdavid2023.01@gmail.com>
2023-09-24 13:14:34 -0400

    update 09/24-3
    update 09/24-3

--- src/test/java/InventoryServiceTest.java
+++ src/test/java/InventoryServiceTest.java
@@ -1,24 +1,60 @@
-//import Model.Inventory;
-//import Service.InventoryService;
-//import org.junit.Assert;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//public class InventoryServiceTest {
-//    InventoryService inventoryService;
-////    runs before every test, to set up the 'premise' of the test (its classes it's testing)
-//
-//    @Before
-//    public void setUp(){
-//        inventoryService = new InventoryService();
-//    }
-//
-//    @Test
-//    public void InventoryServiceItemCanBeFoundTest(){
-//        Inventory item = new Inventory("DIAPERS", 30.00, 10);
-//        inventoryService.(item);
-//        boolean expected = true;
-//        boolean actual = inventoryService.doesItemExist("test item");
-//        Assert.assertEquals(expected, actual);
-//    }
-//}
+package DAO;
+
+import Model.Inventory;
+import Util.ConnectionSingleton;
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class InventoryDAOTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+    Connection conn = ConnectionSingleton.getConnection();
+    InventoryDAO inventoryDAO=new InventoryDAO(conn);
+    Inventory inventory;
+
+    public void testAddItem() {
+        inventory=new Inventory("fruit", 11.1, 100);
+        String item=inventory.getItem();
+        inventoryDAO.addItem(inventory);
+        double actual=inventoryDAO.getItemByName(item).getPrice();
+        double expected=11.1;
+        Assert.assertEquals(actual,expected);
+    }
+    public void testGetItemByName() {
+        String item="fruit";
+        double actual=inventoryDAO.getItemByName(item).getPrice();
+        double expected=11.1;
+        Assert.assertEquals(actual,expected);
+    }
+    public void testUpdateItem() {
+        inventory=new Inventory("fruit", 200);
+        inventoryDAO.updateItem(inventory);
+        String item=inventory.getItem();
+        double actual=inventoryDAO.getItemByName(item).getQuantity();
+        double expected=200;
+        Assert.assertEquals(actual,expected);
+    }
+
+    public void testDeleteItem() {
+        String item="fruit";
+        inventoryDAO.deleteItem(item);
+        Inventory actual=inventoryDAO.getItemByName(item);
+        Inventory expected=null;
+        Assert.assertEquals(actual,expected);
+    }
+    public void testGetAllItems() {
+        List<Inventory> inventoryList = inventoryDAO.getAllItems();
+        Assert.assertNotNull(inventoryList);
+        Assert.assertFalse(inventoryList.isEmpty());
+
+    }
+
+}
